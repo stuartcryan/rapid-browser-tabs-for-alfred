ec92b0fc95e917a4becf85e97b03cd2dfc449f49
Stuart Ryan <stuart@ryan.email>
2015-08-01 22:04:05 +1000

    Committing updated version of activate.rb
    Activate.rb updated to allow opening a new tab rather than simply activating an existing tab.
    
    Signed-off-by: Stuart Ryan <stuart@ryan.email>

--- workflow/actions/activate.rb
+++ workflow/actions/activate.rb
@@ -1,7 +1,21 @@
+#
+# This file originally written by Clinton Strong for the Search Safari and Chrome Tabs Workflow
+# published at http://www.alfredforum.com/topic/236-search-safari-and-chrome-tabs-updated-feb-8-2014/?hl=%2Bsafari+%2Btabs
+# This workflow has been forked from the Feb 8, 2014 release of the workflow and is currently in modified state
+# following the original fork.
+#
+
 Encoding.default_external = Encoding::UTF_8 if defined? Encoding
 
 require File.expand_path('../../browser_tabs', __FILE__)
 
 url = ARGV[0]
 
+if url =~ /^'(.*)'[,]*\s'(.*)'$/
+if $2 == "open"
+url = $1
+exec( 'open "' + $1 + '"' )
+end
+else
 BrowserTabs.activate(url)
+end
