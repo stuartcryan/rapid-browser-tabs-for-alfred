44f31d755ceda6b77cead497959052f6e5cc8e1e
Stuart Ryan <stuart@ryan.email>
2015-08-01 22:03:05 +1000

    Added headers to original forked files
    Headers added to original forked files indicate which files are in a modified state following forking.
    
    Signed-off-by: Stuart Ryan <stuart@ryan.email>

--- workflow/browser_tabs.rb
+++ workflow/browser_tabs.rb
@@ -1,3 +1,10 @@
+#
+# This file originally written by Clinton Strong for the Search Safari and Chrome Tabs Workflow
+# published at http://www.alfredforum.com/topic/236-search-safari-and-chrome-tabs-updated-feb-8-2014/?hl=%2Bsafari+%2Btabs
+# This file has been forked from the Feb 8, 2014 release of the workflow and is currently in an unmodified state
+# with the exception of this comment)
+#
+
 # Allow for local testing without having the gem installed
 $:.unshift File.dirname(__FILE__)
 
